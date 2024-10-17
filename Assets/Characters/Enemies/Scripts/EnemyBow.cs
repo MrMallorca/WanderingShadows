c5af82ef19084674e5ffbaec7e45891d2fcf2fa1
MrMallorca <eduardojosedelgadoguzman@gmail.com>
2024-10-17 10:20:02 +0200

    Enemy bow funcionando

---
+++ Assets/Characters/Enemies/Scripts/EnemyBow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBow : MonoBehaviour
+{
+    Vector3 offset;
+    float arrowForce = 50f;
+
+    public GameObject arrowPrefab;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(-1, 1.3f, 0);
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shooting()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, transform.position + offset, transform.rotation);
+        Rigidbody rb = arrow.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * arrowForce, ForceMode.Impulse);
+        anim.SetBool("isShooting", false);
+    }
+}
